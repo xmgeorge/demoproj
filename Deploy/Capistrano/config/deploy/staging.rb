a1417b3acfaa3d5cc1d6510f61c615285c45fe2d
xmgeorge <george.ulahannan@gmail.com>
2014-01-09 11:29:14 +0530

    added deploy folder files

---
+++ Deploy/Capistrano/config/deploy/staging.rb
@@ -0,0 +1,14 @@
+set :deploy_to, "$HOME/staging"
+
+role :app, "192.168.0.13"
+
+# Email notifications
+set :notify_emails, ["adminteam@xminds.in"]
+
+Notifier.configure do |config|
+  config[:recipient_addresses] = notify_emails
+  config[:subject_prepend] = "[DEPLOY]"
+  config[:site_name] = "wwws.demoproj.xminds.com"
+  config[:email_content_type] = "text/html" # OR "text/plain" if you want the plain text version of the email
+  config[:sections] = %w(deployment release_data source_control latest_release previous_release)
+end
